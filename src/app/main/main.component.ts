a33ccf3f8b94fa19822861b8768ca25ed1b19171
almogon <morales_rivas@hotmail.com>
2021-03-26 23:27:47 +0100

    Add elements using viewChild and renderer service

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {FrameType} from '../shared/enums/frame-type.enum';
-import {BehaviorSubject} from 'rxjs';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -8,38 +6,32 @@ import {BehaviorSubject} from 'rxjs';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  // @ts-ignore
-  frameObserver: BehaviorSubject<FrameType[]> = new BehaviorSubject([]);
-  parFramesList: any[] = [];
   firstIteration = true;
 
-  constructor() {
+  @ViewChild('columns') columns: ElementRef | undefined;
+  @ViewChild('pairElement') pairElement: ElementRef | undefined;
+  @ViewChild('twitch') twitch: ElementRef | undefined;
+  @ViewChild('box') box: ElementRef | undefined;
+
+  constructor(private renderer: Renderer2) {
     this.firstIteration = true;
-    this.frameObserver.next([]);
   }
 
   ngOnInit(): void {
-    this.frameObserver.subscribe(frames => {
-      let auxList: any[] = [];
-      this.parFramesList = [];
-      frames.forEach((value, index) => {
-        auxList.push(value);
-        if ((index + 1) % 2 === 0 && auxList.length > 0) {
-          this.parFramesList.push(auxList);
-          auxList = [];
-        }
-      });
-      if (auxList.length) {
-        const toPush = auxList[0] === FrameType.BOX ? auxList : auxList.concat(FrameType.BOX);
-        this.parFramesList.push(toPush);
-      }
-    });
   }
 
   addFirstElement(): void {
     this.firstIteration = false;
-    this.frameObserver.next([FrameType.TWITCH, FrameType.BOX]);
+    const pairElementAux: ElementRef | undefined = this.pairElement;
+    const twitchBox = this.twitch?.nativeElement;
+    this.renderer.addClass(twitchBox, 'd-flex');
+    this.renderer.appendChild(pairElementAux?.nativeElement, twitchBox);
+
+    const addMoreBox = this.box?.nativeElement;
+    this.renderer.addClass(addMoreBox, 'd-flex');
+    this.renderer.appendChild(pairElementAux?.nativeElement, addMoreBox);
+
+    this.renderer.appendChild(this.columns?.nativeElement, pairElementAux?.nativeElement);
   }
 
   channelLoaded(): void {
@@ -47,27 +39,11 @@ export class MainComponent implements OnInit {
   }
 
   addOtherFrame(): void {
-    const frameList: FrameType[] = this.frameObserver.getValue();
-    frameList[frameList.length - 1] = FrameType.TWITCH;
-    frameList.push(FrameType.BOX);
-    this.frameObserver.next(frameList);
   }
 
-  removeChannel(index: number): void {
-    const frameList: FrameType[] = this.frameObserver.getValue();
-    frameList.slice(index, index + 1);
-    this.frameObserver.next(frameList);
+  removeChannel(): void {
+
   }
 
-  isTwitch(type: FrameType): boolean {
-    return type === FrameType.TWITCH;
-  }
 
-  isBox(type: FrameType): boolean {
-    return type === FrameType.BOX;
-  }
-
-  isEmpty(type: FrameType): boolean {
-    return type === FrameType.EMPTY;
-  }
 }
